c099ac53c9b2f27f53b82248b2b27563f9e6f73b
woodser <woodser@protonmail.com>
2025-02-11 15:05:58 -0500

    add wallet rpc call to get default fee priority

--- src/wallet/wallet_rpc_server_commands_defs.h
+++ src/wallet/wallet_rpc_server_commands_defs.h
@@ -2696,5 +2696,25 @@ namespace wallet_rpc
     typedef epee::misc_utils::struct_init<response_t> response;
   };
 
+  struct COMMAND_RPC_GET_DEFAULT_FEE_PRIORITY
+  {
+    struct request_t
+    {
+      BEGIN_KV_SERIALIZE_MAP()
+      END_KV_SERIALIZE_MAP()
+    };
+    typedef epee::misc_utils::struct_init<request_t> request;
+
+    struct response_t
+    {
+      uint64_t priority;
+
+      BEGIN_KV_SERIALIZE_MAP()
+        KV_SERIALIZE(priority)
+      END_KV_SERIALIZE_MAP()
+    };
+    typedef epee::misc_utils::struct_init<response_t> response;
+  };
+
 }
 }
